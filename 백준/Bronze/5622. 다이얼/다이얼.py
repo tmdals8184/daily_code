67a5337dc3a2a57ff1e8ec2a2d190571ebaccca5
LeeSeungmin <102977104+tmdals8184@users.noreply.github.com>
2025-07-08 18:49:37 +0900

    [Bronze II] Title: 다이얼, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,17 +1,8 @@
-def get_num(val):
-    num = ord(val) - ord("A")
-    if num < 15:
-        result = 2 + num // 3
-    elif num < 19:
-        result = 7
-    elif num < 22:
-        result = 8
-    else:
-        result = 9
-    return result
-
+dial = ["ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"]
 word = input()
 result = 0
 for w in word:
-    result += get_num(w) + 1
+    for idx, d in enumerate(dial):
+        if w in d:
+            result += idx + 3
 print(result)
