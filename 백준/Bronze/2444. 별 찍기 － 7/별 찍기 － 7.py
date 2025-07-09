c234591bc8e367eff78256e28a3d12aa215be5ff
LeeSeungmin <102977104+tmdals8184@users.noreply.github.com>
2025-07-09 10:19:51 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -0,0 +1,14 @@
+n = int(input())
+for i in range(1, n):
+    for _ in range(n - i):
+        print(" ", end="")
+    for _ in range(2 * i - 1):
+        print("*", end="")
+    print()
+for i in range(n):
+    for _ in range(i):
+        print(" ", end="")
+    for _ in range(2 * (n - i) - 1):
+        print("*", end="")
+    print()
+    
